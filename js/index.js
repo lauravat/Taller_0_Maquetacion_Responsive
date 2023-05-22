41450ca011d11cf2a17457d6ad0d61bae743b08a
lauravat <lau3232435124@gmail.com>
2023-05-22 17:44:59 -0500

    desarrollo del footer

--- js/index.js
+++ js/index.js
@@ -1,38 +1,38 @@
-let slideIndex = 1;
-function mostrarDiapositiva(n) {
-    let i;
-    const diapositivas = document.getElementsByClassName("diapositiva");
-    const dots = document.getElementsByClassName("dot");
-    if (n > diapositivas.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = diapositivas.length;
-    }
-    for (i = 0; i < diapositivas.length; i++) {
-        diapositivas[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    diapositivas[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    resetInterval();
-}
-function avanzar(n) {
-    slideIndex += n;
-    mostrarDiapositiva(slideIndex);
-}
-function retroceder(n) {
-    slideIndex -= n;
-    mostrarDiapositiva(slideIndex);
-}
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
-prevButton.addEventListener("click", function () {
-    retroceder(1);
-});
-nextButton.addEventListener("click", function () {
-    avanzar(1);
-});
+document.addEventListener('DOMContentLoaded', function () {
+    var carousel = document.querySelector('.carousel');
+    var inner = carousel.querySelector('.carousel-inner');
+    var items = carousel.querySelectorAll('.carousel-item');
+    var prevButton = carousel.querySelector('.carousel-prev');
+    var nextButton = carousel.querySelector('.carousel-next');
 
+    var currentItem = 0;
+    var itemWidth = items[0].offsetWidth;
+    var itemCount = items.length;
+
+    function navigateCarousel(direction) {
+        if (direction === 'prev') {
+            currentItem--;
+            if (currentItem < 0) {
+                currentItem = itemCount - 1;
+            }
+        } else {
+            currentItem++;
+            if (currentItem >= itemCount) {
+                currentItem = 0;
+            }
+        }
+
+        var translateX = -currentItem * itemWidth;
+        inner.style.transform = 'translateX(' + translateX + 'px)';
+    }
+
+    prevButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        navigateCarousel('prev');
+    });
+
+    nextButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        navigateCarousel('next');
+    });
+});
